7c4a6a6aa5a817d7030eead0cca88017b53519b4
youya66 <nannbann@outlook.jp>
2016-10-27 18:27:26 +0900

    Add progress func

--- pacman/public/javascripts/game.js
+++ pacman/public/javascripts/game.js
@@ -24,68 +24,107 @@
 		return parent;
 	};
 	
-	Game.is_dict = function (obj){
+	Game.is_array = function (arr) {
+		return toString.call(arr) === "[object Array]";
+	}
+
+	Game.is_dict = function (obj) {
 		return toString.call(obj) === "[object Object]";
 	};
 
-	Game.init_assets = function (obj) {
-		if(!Game.is_dict(obj)){
+	Game.init_assets = function (arr) {
+		if(!Game.is_array(arr)){
 			return false;
 		}
 		Game.namespace("Game.assets.collections");
 		let collections = Game.assets.collections = [];
 
-		for(let key in obj){
-			collections.push(obj[key]);
+		for(let i = 0, len = arr.length; i < len; i += 1){
+			collections.push(arr[i]);
 		}
 	};
 
-	Game.load_first = function (assets) {
-		for(let i = 0, length = load_init.length; i < length; i += 1){
+	Game.load = function (_time) {
+		let length = Game.assets_state.fetch_assets_length();
+		let time = _time || 0;
+
+		Game.progress_render.store_interval(time);
+		Game.progress_render.draw();
+
+		for(let i = 0; i < length; i += 1){
 			Game.namespace("Game.assets.images");
 			let images = Game.assets.images = [];
+			let assets = Game.assets.collections[i];
+
 			images[assets.name] = new Image();
 			images[assets.name].src = assets.src;
-			images[assets.name].onload = Game.progress_render();
+			images[assets.name].onload = Game.progress_render.draw();
 		}
-		let length = Game.operate_progress.store_assets_length();
-		Game.progress_render(len);
 	};
 
-	Game.operate_progress = (function () {
+	Game.assets_state = (function () {
 		let length = 0;
 		let i = 0;
 		
-		function store_assets_length() {
-			len = Game.assets.collections.length;
-			return len;
+		function fetch_assets_length() {
+			try{
+				length = Game.assets.collections.length;
+			}catch(e) {
+				length = 0;
+			}
+			return length;
 		}
-		function loaded_assets() {
+		function loaded_assets_parcent() {
 			i = i + 1;
 			let parcent = (i / length) * 100;
 			return parcent;
 		}
 		return {
-			store_assets_length,
-			loaded_assets
+			fetch_assets_length,
+			loaded_assets_parcent
 		};
 	})();
 
-	Game.progress_render = (function () {
+	Game.progress_render = (function (self) {
 		let progress_times = 0;
-		let i = 0;
-		return function (len) {
-			progress_times += 1;
-			for(; i < len; i += 1){
-				progress_update();
-				break;
+		let last_date = new Date().getTime();
+		// ロード時間の最小値
+		let load_min_time = 300;
+		let interval = 0;
+
+		let store_interval = function (time) {
+			if(time >= load_min_time){
+				load_min_time = time;
 			}
-			function progress_update() {
+			let length = self.assets_state.fetch_assets_length();
+			interval = load_min_time / length;
+		}
+
+		let draw = function () {
+			let now = new Date().getTime();
+			progress_times += 1;
+
+			if(last_date + interval <= now){
+				while(progress_times >= 1){
+					last_date = now;
+					progress_times -= 1;
+					console.log(self.assets_state.loaded_assets_parcent());
+					break;
+				}
+			}else{
+				let diff = now - (last_date + interval);
+				setTimeout(draw, diff);
 			}
 		}
-	})();
 
-	Game.fetch_cell_point = function(w,h,x,y) {
+		return {
+			store_interval,
+			draw
+		}
+
+	}(Game));
+
+	Game.fetch_image_point = function(w,h,x,y) {
 	    let ix = 0;
 	    let iy = 0;
 	    for(;iy <= h; iy += y){
--- pacman/public/javascripts/main.js
+++ pacman/public/javascripts/main.js
@@ -8,9 +8,11 @@ window.addEventListener("DOMContentLoaded", () => {
 	root.canvas.width = 250;
 	root.canvas.height = 250;
 	root.context = root.canvas.getContext("2d");
+
+	Game.load(300);
 });
 
-Game.init_assets({
+Game.init_assets([{
 	name: "player",
 	type: "image",
 	src: "./images/player.png"
@@ -18,4 +20,4 @@ Game.init_assets({
 	name: "dungeon",
 	type: "image",
 	src: "./images/dungeon.png"
-});
+}]);
